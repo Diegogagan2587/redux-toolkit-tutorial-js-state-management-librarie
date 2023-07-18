513a0a129d1b21045a8c847ae1256644ad4dcffa
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-17 22:06:40 -0600

    add Navbar.jsx component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { CartIcon } from "../icons";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="nav-center">
+                <h3>
+                    redux toolkit
+                </h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
