477492e2daf76d52e6d026ab589328b82420703b
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-18 15:38:40 -0600

    Import and implement modal within the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import Modal from "./components/Modal";
 import { useSelector, useDispatch } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
 import { useEffect } from "react";
+
 
 function App() {
   const { cartItems} = useSelector((store)=>store.cart)
@@ -14,6 +16,7 @@ function App() {
 
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
