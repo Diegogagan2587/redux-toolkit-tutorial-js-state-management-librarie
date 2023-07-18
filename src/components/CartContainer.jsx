eeb1fc7d4cc91c8c6d82978078f3c5b1a31b3fb3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-18 13:56:42 -0600

    add Componenet: Cartcontainer

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,7 +1,43 @@
+import cartItems from "../cartItems";
 import CartItem from "./Cartitem";
+import { useSelector } from "react-redux";
 
 const CartContainer = () => {
-  return <div>CartContainer</div>;
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2> Your Bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+  return (
+    <>
+      <section className="cart">
+        <header>
+          <h2>Your bag</h2>
+        </header>
+        <div>
+          {cartItems.map((item) => {
+            return <CartItem key={item.id} {...item} />;
+          })}
+        </div>
+        <footer>
+          <hr />
+          <div className="cart-total">
+            <h4>
+              Total <span>$</span>
+              {total}
+            </h4>
+          </div>
+          <button className="btn clear-btn">Clear Cart</button>
+        </footer>
+      </section>
+    </>
+  );
 };
 
 export default CartContainer;
