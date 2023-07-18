652a855e0c41a0317e8ea3bbca5162f86d34a431
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-18 15:28:31 -0600

    Implement function, Calculate Total amount from all itmes in the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
 
 function App() {
+  const { cartItems} = useSelector((store)=>store.cart)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(calculateTotals())
+  },[cartItems])
+
   return (
     <main>
       <Navbar />
