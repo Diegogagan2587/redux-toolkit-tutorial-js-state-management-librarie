093e27be27792f3dbc65ef5f557f582d0315b223
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-18 14:47:59 -0600

    add reducers:ClearCArt(), removeItem()

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,7 +10,19 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => {
+        return item.id !== itemId;
+      });
+    },
+  },
 });
 
-// console.log(cartSlice);
+//  console.log(cartSlice);
+export const { clearCart, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
