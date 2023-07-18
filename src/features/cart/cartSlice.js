fcfd2e1d5fd3115a35a44fe9fb230691e2b96dfa
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-18 15:01:12 -0600

    add reducers: Increment item, decrement items

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -20,9 +20,17 @@ const cartSlice = createSlice({
         return item.id !== itemId;
       });
     },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
 //  console.log(cartSlice);
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 export default cartSlice.reducer;
